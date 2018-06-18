2d5461ca2ea2c01e1f34e96746c9e3b3ea2b9819
h5 <sehe89@gmail.com>
2018-06-18 10:59:25 +0200

    fix for cloning gitrepo

--- magic.pl
+++ magic.pl
@@ -3,12 +3,19 @@
 use strict;
 use warnings;
 
-my $pfad = '/var/www/html';
+use Git::Repository;
+
+########################################
+my $document_root = '/var/www/html';
+my $instance_dir = $pfad . '/' . $instance_name;
+my $git_url = 'https://github.com/huhn511/rails-docker-sample.git';
+
 my $instance_name = $ARGV[0];
 chomp($instance_name);
 
 printf($instance_name . "\n");
+########################################
+system("mkdir -p $document_root/$instance_name");
 
-system("mkdir -p $pfad/$instance_name");
-system("cd $pfad/$instance_name");
-system("git clone https://github.com/huhn511/rails-docker-sample.git");
+Git::Repository->run( clone => $git_url => $instance_dir );
+my $repo = Git::Repository->new( work_tree => $instance_dir );
