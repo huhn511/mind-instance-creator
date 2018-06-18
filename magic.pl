f0ff4af0cdc04161bb414375b3ee826cc1286d7c
h5 <sehe89@gmail.com>
2018-06-18 11:16:58 +0200

    checks if a directory exists. Abort if so

--- magic.pl
+++ magic.pl
@@ -1,21 +1,28 @@
 #!/usr/bin/perl
+#cpan install Git::Repository;
+
 
 use strict;
 use warnings;
-
 use Git::Repository;
 
 ########################################
 my $document_root = '/var/www/html';
-my $instance_dir = $pfad . '/' . $instance_name;
 my $git_url = 'https://github.com/huhn511/rails-docker-sample.git';
 
 my $instance_name = $ARGV[0];
 chomp($instance_name);
 
+my $instance_dir = $document_root . '/' . $instance_name;
+
 printf($instance_name . "\n");
 ########################################
-system("mkdir -p $document_root/$instance_name");
 
+if(-d $instance_dir){
+  printf("Directory already exists! Choose another name!\n");
+  return 0;
+}else{
+  system("mkdir -p $document_root/$instance_name");
+}
 Git::Repository->run( clone => $git_url => $instance_dir );
 my $repo = Git::Repository->new( work_tree => $instance_dir );
